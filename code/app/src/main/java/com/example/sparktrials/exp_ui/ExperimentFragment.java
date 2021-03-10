0ffa5063083391aa6d2304ef7d449297834dcceb
mccunard <mccunard1@gmail.com>
2021-03-09 18:56:05 -0700

    Added packages and files for the experiment home page and its tabs
    I'm currently thinking i'll rework the experiment activity into a fragment which will have TabViews for each of the options on the page, but i'm still working stuff out. This would fix the problem I was having with how to overlay it on the Main Activity as it would just work like the other Home fragments, but with more going on inside it. and we already know how to pass data, like experiment ID and user ID to a fragment so it also fixes that issue

---
+++ code/app/src/main/java/com/example/sparktrials/exp_ui/ExperimentFragment.java
@@ -0,0 +1,16 @@
+package com.example.sparktrials.exp_ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExperimentFragment extends AppCompatActivity {
+
+    public void onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+
+    }
+}
---
+++ code/app/src/main/java/com/example/sparktrials/exp_ui/action/ActionViewModel.java
@@ -0,0 +1,4 @@
+package com.example.sparktrials.exp_ui.action;
+
+public class ActionViewModel {
+}

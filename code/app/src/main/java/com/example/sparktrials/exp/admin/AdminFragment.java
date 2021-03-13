870dd3f8b745bda90bb62c544f13c5924fb54206
Tomas <tomas.peschke@gmail.com>
2021-03-13 14:33:36 -0700

    Tabs in Experiment Main
    Will add database functionality as soon as philip is done with his class

--- code/app/src/main/java/com/example/sparktrials/exp/admin/AdminFragment.java
+++ code/app/src/main/java/com/example/sparktrials/exp/admin/AdminFragment.java
@@ -1,4 +1,25 @@
 package com.example.sparktrials.exp.admin;
 
-public class AdminFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.sparktrials.R;
+
+public class AdminFragment extends Fragment {
+    View view;
+    public AdminFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_admin, container, false);
+        return view;
+    }
 }

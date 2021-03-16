4e6bd5580449d0b4120fd98ebd949deaa2b3d718
AlexSorx <sorore@ualberta.ca>
2021-03-15 21:40:17 -0600

    ignore

--- code/app/src/main/java/com/example/sparktrials/main/me/MeFragment.java
+++ code/app/src/main/java/com/example/sparktrials/main/me/MeFragment.java
@@ -1,6 +1,8 @@
 package com.example.sparktrials.main.me;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,8 +19,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.sparktrials.FirebaseManager;
 import com.example.sparktrials.IdManager;
 import com.example.sparktrials.R;
+import com.example.sparktrials.main_ui.publish.PublishFragmentManager;
 import com.example.sparktrials.models.Profile;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -30,16 +34,20 @@ import java.util.Map;
 public class MeFragment extends Fragment {
 
 
-    EditText etName, etContact;
-    Profile userProfile;
+    EditText et_name, etContact;
     Button updateButton;
-    String userID = "726c77d8-54c7-41a1-a149-afe608892add";
     String name, cellphone;
-    TextView tvUserID;
+    TextView tvUserID, tvName, tvContact;
+    IdManager idManager = new IdManager();
+    String userID;
 
-    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-
-
+    /**
+     * on Create view creates the view when fragment is clicked
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return root view
+     */
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_me, container, false);
@@ -47,104 +55,78 @@ public class MeFragment extends Fragment {
         return root;
     }
 
-    public MeFragment() {
-
-        firestore.collection("users").document(userID)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        assert document != null;
-                        name= (String) document.get("name");
-                        etName = getView().findViewById(R.id.et_name);
-                        etName.setText(name);
-                        cellphone = (String) document.get("contact");
-                        etContact = getView().findViewById(R.id.et_contact);
-                        etContact.setText(cellphone);
-
-
-
-                        Log.d("Data", document.getId() + " => " + document.getData());
-
-                    }
-                });
-    }
-
-
-
-
+    /**
+     * When the view is created this method initializes all the necessary values and handles button clicking
+     * @param view
+     * @param savedInstanceState
+     */
     public void onViewCreated(View view , Bundle savedInstanceState){
+
+        userID=  idManager.getUserId(this.getActivity());
+
         // Set user id text view
         tvUserID= getView().findViewById(R.id.user_id);
         tvUserID.setText(userID);
 
         // Set username
-        etName = getView().findViewById(R.id.et_name);
-        etName.setText(name);
-
-
+        tvName = getView().findViewById(R.id.tv_name);
+        tvName.setText(name);
         // Set contact
-        etContact = getView().findViewById(R.id.et_contact);
-        etContact.setText(cellphone);
+        tvContact = getView().findViewById(R.id.tvContact);
+        tvContact.setText(cellphone);
 
         // initialize update button
-        updateButton = getView().findViewById(R.id.btn_up);
+        updateButton = getView().findViewById(R.id.btn_ep);
 
+
+        /**
+         * Method to check when a user clicks update profile
+         */
         updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateProfile();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                View updateMe = getLayoutInflater().inflate(R.layout.update_me_fragment, null);
+                builder.setView(updateMe);
+                    builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            et_name = updateMe.findViewById(R.id.et_name);
+                            etContact= updateMe.findViewById((R.id.etContact));
+                            updateProfile();
+                            Toast.makeText(getContext() , "updated", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext() , "Nevermind", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
-
-
-
     }
-
-
-
-
-
-
-
-
 
     /** Update Profile Method
-     * Checks to make sure data is input
-     *
-     *
+     * Checks to make sure data is input and then updates data to the database
      */
     public void updateProfile() {
-        Map<String,Object> data = new HashMap<>();
-        DocumentReference dRef = FirebaseFirestore.getInstance().collection("users").document("726c77d8-54c7-41a1-a149-afe608892add");
-
-
-        String nameInput = etName.getText().toString();
-        if (!nameInput.isEmpty() ){
-            data.put("name",nameInput);
-            dRef.set(data);
-
-        }else{
-            Toast.makeText(this.getContext(), "Please enter a valid name", Toast.LENGTH_SHORT).show();
-        }
-
-
+        userID=  idManager.getUserId(this.getActivity());
+        FirebaseManager firebaseManager = new FirebaseManager();
+        String nameInput = et_name.getText().toString();
         String contactInput = etContact.getText().toString();
+        if (!nameInput.isEmpty() ){
 
-        if (!contactInput.isEmpty() ){
-            data.put("contact",contactInput);
-            dRef.set(data);
+            firebaseManager.update("users", userID, "name", nameInput );
+            tvName.setText(nameInput);
 
-        }else{
-            Toast.makeText(this.getContext(), "Please enter a valid contact", Toast.LENGTH_LONG).show();
         }
-
-
-
-
-        Log.d("Button", "update profile clicked");
+        if (!contactInput.isEmpty() ){
+            firebaseManager.update("users", userID, "contact", contactInput );
+            tvContact.setText(contactInput);
+        }
     }
-
-
 }
 

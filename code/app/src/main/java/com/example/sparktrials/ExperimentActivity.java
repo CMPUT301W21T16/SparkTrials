00267607342f1cf4e83e9749978d5f2d888e6f15
Tomas <tomas.peschke@gmail.com>
2021-03-17 20:00:00 -0600

    Made experiment header more compact and aesthetic

--- code/app/src/main/java/com/example/sparktrials/ExperimentActivity.java
+++ code/app/src/main/java/com/example/sparktrials/ExperimentActivity.java
@@ -3,6 +3,7 @@ package com.example.sparktrials;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -65,6 +66,7 @@ public class ExperimentActivity extends AppCompatActivity {
         backToMain = findViewById(R.id.back_button);
         titleText = findViewById(R.id.text_title);
         descText = findViewById(R.id.text_desc);
+        descText.setMovementMethod(new ScrollingMovementMethod());
 
         tablayout = (TabLayout) findViewById(R.id.tablayout_id);
         viewPager = (ViewPager) findViewById(R.id.viewpager_id);

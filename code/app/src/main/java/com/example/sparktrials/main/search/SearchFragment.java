3f6d8547a1e3e93569a4eb1aadb727f880cb17ad
Tomas <tomas.peschke@gmail.com>
2021-03-25 13:07:16 -0600

    search ui refurbished

--- code/app/src/main/java/com/example/sparktrials/main/search/SearchFragment.java
+++ code/app/src/main/java/com/example/sparktrials/main/search/SearchFragment.java
@@ -14,6 +14,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -43,7 +44,7 @@ public class SearchFragment extends Fragment {
     private ListView searchListView;
 
     private EditText searchBar;
-    private Button searchButton;
+    private ImageButton searchButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {

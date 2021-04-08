5b956e5fa041b4489476b7395daac19748ec7de1
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2021-04-08 13:58:42 -0600

    AdminFragment Test Documentation

--- code/app/src/androidTest/java/com/example/sparktrials/exp/admin/AdminFragmentTest.java
+++ code/app/src/androidTest/java/com/example/sparktrials/exp/admin/AdminFragmentTest.java
@@ -34,6 +34,11 @@ public class AdminFragmentTest {
     public ActivityTestRule<MainActivity> rule =
             new ActivityTestRule<>(MainActivity.class, true, true);
 
+    /**
+     * Gos to the profile tab and gets the user ID, then publishes the experiment used for testing.
+     * Then goes to the admin tab in experiment activity
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception{
         db = FirebaseFirestore.getInstance();
@@ -48,28 +53,44 @@ public class AdminFragmentTest {
         spinner.setSelection(0, true);
         solo.enterText((EditText) solo.getView(R.id.expTitle_editText), expTitle);
         solo.clickOnButton("POST");
+        //Search for the test experiment
         solo.clickOnView(solo.getView(R.id.navigation_search));
         solo.enterText((EditText) solo.getView(R.id.search_bar),expTitle);
         solo.clickInList(0,0);
+        //Go to the admin tab
         solo.clickOnScreen(900,600);
         solo.sleep(1000);
     }
+
+    /**
+     * Ends the experiment then clicks on the action tab and make sure it displays the closed experiment UI
+     */
     @Test
     public void testEndExperiment(){
+        //Ends the experiment
         solo.clickOnView(solo.getView(R.id.end_experiment_button));
         solo.sleep(1000);
+        //Clicks on the action tab
         solo.clickOnScreen(150,600);
         solo.sleep(1000);
+        //Makes sure that the UI displays that the experiment is closed
         assertTrue(solo.searchText("Experiment is Closed"));
     }
 
+    /**
+     * Makes sure that when the unpublish button is clicked, the buttons text changes to "Publish Experiment"
+     * and that also the experiemnt does not appear in the search bar
+     */
     @Test
     public void testUnPublish(){
+        //Clicks on the unpublish experiment button
         solo.clickOnView(solo.getView(R.id.unpublish_experiment_button));
         solo.sleep(1000);
         solo.clickOnButton("YES");
+        //Makes sure that the button text changes
         assertTrue(solo.searchText("Publish Experiment"));
         solo.clickOnView(solo.getView(R.id.back_button));
+        //Searches for the experiment and makes sure it doesnt appear
         solo.clickOnView(solo.getView(R.id.navigation_search));
         solo.enterText((EditText) solo.getView(R.id.search_bar),expTitle);
         solo.clickOnView(solo.getView(R.id.search_button));
@@ -77,6 +98,10 @@ public class AdminFragmentTest {
         assertFalse(solo.searchText(expTitle));
     }
 
+    /**
+     * Deletes all the experiments used for testing
+     * @throws InterruptedException
+     */
     @After
     public void deleteExperiment() throws InterruptedException {
         CollectionReference expCollection = db.collection("experiments");
